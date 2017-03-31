77d2eb054cce50fd64de9c3e9cd056a40d0af0b8
00000H <753755601@qq.com>
2017-03-31 16:09:32 +0800

    使用相对路径

--- loadData/loadRRFile.m
+++ loadData/loadRRFile.m
@@ -1,6 +1,6 @@
 function [ rrNumber, rrType, rrintervalTime ] = loadRRFile( dataNumber )
 %% 导入RR文件
-filename = fullfile('.', 'data', 'arrtfn', [num2str(dataNumber), '.arr']);
+filename = fullfile('data', 'arrtfn', [num2str(dataNumber), '.arr']);
 fileID = fopen(filename);
 rrData = textscan(fileID, '%f %s %f');
 rrintervalTime = rrData{3};
