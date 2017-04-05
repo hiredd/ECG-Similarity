93b4bb8299f82e2da6f148803c4f3f7e10e34b9a
00000H <753755601@qq.com>
2017-04-05 11:21:33 +0800

    使用rr间距的比例作为qs分割的依据

--- similarity/countDTW.m
+++ similarity/countDTW.m
@@ -1,5 +1,5 @@
 function [ dist ] = countDTW( wave1, wave2 )
-%计算两个波的dwt距离
+% 计算两个波的dwt距离
 dist = dtw(wave1, wave2);
 end
 
--- similarity/findQSTest.m
+++ similarity/findQSTest.m
@@ -1,5 +1,5 @@
 allFileNumber = getAllFileNumber();
-% parfor i=1:length(allFileNumber)
+% parfor i =1 : length(allFileNumber)
 %     dataNumber = allFileNumber(i);
 %     display(['ÎÄ¼þºÅ£º' num2str(dataNumber)]);
 %     [waveData, ~] = loadDatFile(dataNumber);
@@ -18,8 +18,8 @@ dataNumber = 102;
 hold on;
 plot(waveData);
 lenOfRR = length(rrNumber);
-plot(qwaves(1:lenOfRR), waveData(qwaves(1:lenOfRR)), 'rx','MarkerFaceColor', 'g')
+plot(qwaves(2:length(rrNumber)-1), waveData(qwaves(2:length(rrNumber)-1)), 'rx','MarkerFaceColor', 'g')
 plot(rrNumber, waveData(rrNumber), 'bv','MarkerFaceColor', 'y')
-plot(swaves(1:lenOfRR), waveData(swaves(1:lenOfRR)), 'rx', 'MarkerFaceColor', 'g')
+plot(swaves(2:length(rrNumber)-1), waveData(swaves(2:length(rrNumber)-1)), 'rx', 'MarkerFaceColor', 'g')
 grid on;
 legend('ECG Signal', 'Q-waves', 'R-waves', 'S-waves');
